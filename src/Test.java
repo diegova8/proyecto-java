343dc5cb0a79896694fa65471eb498ddab68d0fb
diegova8 <diegova8@gmail.com>
2017-04-02 17:05:17 -0600

    test file added

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
+
+public class Test {
+	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintStream out = System.out;
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "hola mundo!");
+		
+	}
+
+}
