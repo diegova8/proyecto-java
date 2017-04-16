d5c42937348a46a7a4601101b09243c4dea3dc23
Gary Valverde Hampton <garyhampton.12@gmail.com>
2017-04-16 17:46:40 -0600

    Caso Class update

--- src/cr/ac/ucenfotec/sistemajudicial/classes/Caso.java
+++ src/cr/ac/ucenfotec/sistemajudicial/classes/Caso.java
@@ -9,7 +9,81 @@ public class Caso {
 	private String 		descripcion;
 	private Quellerarte quellerarte;
 	private Juez 		juez;
-	private Estado 		estado;
+	private enum		estado {
+		Recibido, 
+		Aceptado, 
+		Consulta, 
+		Rechazado, 
+		Redactado, 
+		Resuelto, 
+		Revision
+	}
 	private LocalDate 	fecha;
-	private ArrayList	historial;
+	private String	historial;
+	//Constructor
+	public Caso() {
+		super();
+		// TODO Auto-generated constructor stub
+	}	
+	public Caso(int iD, String numero, String descripcion, Quellerarte quellerarte, Juez juez, LocalDate fecha,
+			String historial) {
+		super();
+		ID = iD;
+		this.numero = numero;
+		this.descripcion = descripcion;
+		this.quellerarte = quellerarte;
+		this.juez = juez;
+		this.fecha = fecha;
+		this.historial = historial;
+	}
+
+
+	//Getters y Setters
+	public int getID() {
+		return ID;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public Quellerarte getQuellerarte() {
+		return quellerarte;
+	}
+	public Juez getJuez() {
+		return juez;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public String getHistorial() {
+		return historial;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public void setQuellerarte(Quellerarte quellerarte) {
+		this.quellerarte = quellerarte;
+	}
+	public void setJuez(Juez juez) {
+		this.juez = juez;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public void setHistorial(String historial) {
+		this.historial = historial;
+	}
+	
+	
+	
+	
+	
 }
